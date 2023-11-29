b78b7adb21ea9c44cd589d9d36ff03446d6df34c
Andreotti Ludovic <andr.ludow@gmail.com>
2023-11-29 10:52:09 +0100

    add legales mentions file

---
+++ src/pages/legales mentions/Legalmentions.jsx
@@ -0,0 +1,37 @@
+/**
+ * @returns {React.node}
+ */
+export const Legalesmentions = () => {
+  return (
+    <>
+      <div>
+        <h3>Informations legales</h3>
+        <span>Andreotti Ludovic</span>* 72 rue alphonse daudet, 30210 remoulins
+        06 42 53 39 53 andr.ludow@gmail.com siret : 85209615500019
+      </div>
+      <div>
+        <h3>Créateur du site internet</h3>
+        <span>Andreotti Ludovic</span>
+        contact : andr.ludow@gmail.com
+      </div>
+      <div>
+        <h3>Hebergeur du site </h3>
+        <span>OVH</span>SAS au capital de 10 174 560 € RCS Lille Métropole 424
+        761 419 00045 Code APE 2620Z N° TVA : FR 22 424 761 419 Siège social : 2
+        rue Kellermann - 59100 Roubaix - France OVH SAS est une filiale de la
+        société OVH Groupe SA, société immatriculée au RCS de Lille sous le
+        numéro 537 407 926 sise 2, rue Kellermann, 59100 Roubaix. Directeur de
+        la publication : Michel Paulin Ce site est hébergé par OVH SAS 2 rue
+        Kellermann - 59100 Roubaix - France
+      </div>
+      <div>
+        <h3>Propriété intellectuelles</h3>
+        <p>
+          Les images, dessins et autres créations utilisés dans ce site internet
+          est la propriété de Andreotti ludovic et sont des créations
+          originales. @tout droits réservés
+        </p>
+      </div>
+    </>
+  );
+};
