ed9fda7931d0f6998461962c5cb8db4f0a57f202
Andreotti Ludovic <andr.ludow@gmail.com>
2023-11-30 14:05:52 +0100

    feat: add picture to illustration page

--- src/pages/illustration/imageAPI/images.jsx
+++ src/pages/illustration/imageAPI/images.jsx
@@ -1,49 +1,103 @@
-import hypolove from "./../illustration/hypolove.jpg";
-import dessinSeb from "./../illustration/dessinseb.jpg";
-import illu_om from "./../illustration/illu_om.jpg";
-import readToEscape from "./../illustration/readtoescape.png";
-import b_book from "./../illustration/B_book_04.jpg";
+import { bureau3D } from "./../illustration/bureau3d.jpg";
+import { lit3D } from "./../illustration/chambre3d.png";
+import { dragons } from "./../illustration/fourth Wing.jpg";
+import { hommeetchat } from "./../illustration/hommeetchat.jpg";
+import { hypolove } from "./../illustration/Hypolove.jpg";
+import { inktober1 } from "./../illustration/inktober 01.jpg";
+import { inktober13 } from "./../illustration/inktober 13.jpg";
+import { inktober14 } from "./../illustration/inktober14.jpg";
+import { pokeball96 } from "./../illustration/pokeball9-6.png";
+import { pokecenter } from "./../illustration/pokecenter.jpg";
+import { readtoescape } from "./../illustration/read to escape.png";
+import { supportermarseillais } from "./../illustration/supportermarseillais.jpg";
+import { variouscrew } from "./../illustration/variouscrew.png";
+import { wallpaperpokemon } from "./../illustration/Wallpaper_pokemon.jpg";
+import { wallpaperpokemon2 } from "./../illustration/Wallpaper_pokemon_phne.png";
 export const IMAGES = [
   {
-    title: "hypolove",
+    title: "Bureau en 3D",
+    source: bureau3D,
+    description:
+      "Afin de découvrir le logiciel BLENDER, j’ai voulu réinterpréter un affiche d’ameublement.",
+  },
+  {
+    title: "Lit en 3D",
+    source: lit3D,
+    description:
+      "Cette chambre est une représentation en 3D. C’était un bon entrainement pour les matières en tissus.",
+  },
+  {
+    title: "Dragons",
+    source: dragons,
+    description:
+      "Illustration créée pour une Book-stragameuse, le défis était d’illustrer un publication sur le thème du livre ‘Fourth Wing’",
+  },
+  {
+    title: "Homme et son chat",
+    source: hommeetchat,
+    description:
+      "Cette illustration est un dessin offert à l'occasion d’un anniversaire. Il représente un homme chasseur de pokémon et son compagnon chat.",
+  },
+  {
+    title: "Hypolove",
     source: hypolove,
     description:
-      "Ce dessin représente un hypocampe, ce fut le premier dessin que j'ai créer sur le logiciel Procreate",
+      "Le premier dessin que j’ai réalisé sur le logiciel procreate. Il représente un hippocampe.",
   },
   {
-    title: "Dompteur de poket Monster",
-    source: dessinSeb,
+    title: "Inktober - Rêve",
+    source: inktober1,
     description:
-      "Cette illustration est un cadeaux d'anniverssaire, il fait référence à pokemon.",
+      "Dessin réalisé à l'occasion de l’Inkctober ( événement organisé en octobre, durant ce mois nous devons réaliser un dessin par jour). Ce dessin est une représentation abstraite d’un rêve. ",
   },
   {
-    title: "Allez l'OM",
-    source: illu_om,
+    title: "Inktober - s'élever",
+    source: inktober13,
     description:
-      "Cette création est une caricature d'un supporter du club de foot Marseillais.",
+      "Dessin réalisé à l'occasion de l’Inkctober ( événement organisé en octobre, durant ce mois nous devons réaliser un dessin par jour).Ce dessin fait référence au mot ‘s’élever’. ",
   },
   {
-    title: "REad to Escape",
-    source: readToEscape,
+    title: "Inktober - Chateau",
+    source: inktober14,
     description:
-      "Tout est dans le titre, une inspiration dans laquelle certaines personne pourrais se reconnaitre.",
+      "Dessin réalisé à l'occasion de l’Inkctober ( événement organisé en octobre, durant ce mois nous devons réaliser un dessin par jour).Ce dessin est un château abstrait. ",
   },
   {
-    title: "Book Trop theory",
-    source: b_book,
-    description:
-      "Dans le but de créer une couverture de livre, cette illustration représente les personnages principaux de ce livre",
+    title: "Pokéballs",
+    source: pokeball96,
+    description: "Représentations en 3D de pokéballs.",
   },
   {
-    title: "Book Trop theory 2",
-    source: b_book,
+    title: "Pokécenter",
+    source: pokecenter,
     description:
-      "Dans le but de créer une couverture de livre, cette illustration représente les personnages principaux de ce livre",
+      "Cette image est une représentation 3D stylisée d’un point de vue sur un centre pokémon.",
   },
   {
-    title: "Book Trop theory 3",
-    source: b_book,
+    title: "Read to Escape",
+    source: readtoescape,
     description:
-      "Dans le but de créer une couverture de livre, cette illustration représente les personnages principaux de ce livre",
+      "Dessin rapide suite à une idée que j’ai eu sur le moment.Il représente un jeune femme protégée de la pluie par un livre.",
+  },
+  {
+    title: "Supporter Marseillais",
+    source: supportermarseillais,
+    description:
+      "Ce dessin représente un homme, supporter d’un club de l’OM avec en fond le vélodrome.",
+  },
+  {
+    title: "Various Crew",
+    source: variouscrew,
+    description: "Bannière réalisée pour le streamer Various crew.",
+  },
+  {
+    title: "Pokeballs Wallpaper",
+    source: wallpaperpokemon,
+    description: "Fond d’écran des trois pokéballs réalisées en 3D.",
+  },
+  {
+    title: "Pokeballs Wallpaper 2",
+    source: wallpaperpokemon2,
+    description: "Fond d’écran des trois pokéballs réalisées en 3D.",
   },
 ];
